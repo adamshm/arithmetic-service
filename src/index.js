58fea2dba2f5c8be939fddc8fa1787bf43e9f7a0
adamshm <adamshm@iu.edu>
2024-02-15 17:47:19 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -12,4 +12,23 @@ app.get('/add/:n/:m', (req, res) => {
     res.json(Number(req.params.n) + Number(req.params.m));
 });
 
+app.get('/subtract/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) - Number(req.params.m));
+});
+
+app.get('/multiply/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) * Number(req.params.m));
+});
+
+app.get('/divide/:n/:m', (req, res) => {
+    const dividend = Number(req.params.n);
+    const divisor = Number(req.params.m);
+
+    if (divisor === 0) {
+        res.status(400).json({ error: 'Division by zero is not allowed' });
+    } else {
+        res.json(dividend / divisor);
+    }
+});
+
 app.listen(port);
